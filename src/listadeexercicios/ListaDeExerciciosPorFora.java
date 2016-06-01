01e0b6fe19e05f524e328fe523c3aab7a2d2275c
Aluno <Aluno@192.68.0.80>
2016-06-01 10:01:19 -0300

    atualização

--- src/listadeexercicios/ListaDeExerciciosPorFora.java
+++ src/listadeexercicios/ListaDeExerciciosPorFora.java
@@ -5,31 +5,36 @@ import java.util.Scanner;
 public class ListaDeExerciciosPorFora {
 
     static Scanner ler = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        
+
         System.out.print("Digite o número do exercício > ");
         String exercicio = ler.next();
-        
-        switch(exercicio){
-            case "1":
-                fibonacci();
-                break;
-    }
-        
-    }
 
-    private static void fibonacci() {
-        
-        System.out.println("Fibonacci\n");
-        
-        int valor [] = new int[12];
-        
-        for(int i = 1; i<12; i++){
-            
-            System.out.println(valor+""+valor);
-            
+        switch (exercicio) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            case "Exercicios por fora":
+                break;
+
+            default:
+                System.out.println("Lista não existe...");
+                break;
+
         }
     }
-    
 }
